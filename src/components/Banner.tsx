73f515c4c0d50b49c38c76fdbbcffb79de24deeb
Victor Pontes <victorpontes2013@gmail.com>
2024-01-04 23:49:07 -0700

    Fix loading issue

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,18 +1,27 @@
 import Image from "react-bootstrap/Image";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import bannerImage from "../images/banner.jpg"
 import "./Banner.css"
 
 function Banner() {
+    const [isImageRendered, setImageRendered] = useState(false);
+
+    useEffect(() => {
+        // Set the flag to true after the first component has fully rendered
+        setImageRendered(true);
+    }, []); // Empty dependency array means this effect runs once after the initial render
+
     return (
         <div className="parentContainer">
             <Image className="banner" src={bannerImage} fluid/>
-            <div className="centered">
-                <div className="bannerText">
-                    <p className="superscript">Hi, I'm</p>
-                    <h1 className="headerText">Victor</h1>
+            {isImageRendered && (
+                <div className="centered">
+                    <div className="bannerText">
+                        <p className="superscript">Hi, I'm</p>
+                        <h1 className="headerText">Victor</h1>
+                    </div>
                 </div>
-            </div>
+            )}
         </div>
     );
 }
