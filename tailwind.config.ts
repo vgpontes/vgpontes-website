3ab2e4894cf74aa13698cd63b6c07325c2afffe6
Victor Pontes <victorpontes2013@gmail.com>
2024-06-21 00:39:55 -0700

    add title splash

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import * as colors from "tailwindcss/colors"
 
 const config: Config = {
   content: [
@@ -7,13 +8,16 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    extend:{
+      colors: {
+        'dark-bg': '#1A1A24'
       },
-    },
+      spacing: {
+        '515': '515px',
+        '90': '90px',
+        '797': "797px"
+      }
+    }
   },
   plugins: [],
 };
